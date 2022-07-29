a20cd8742d37d6af04f01417e5dde5e653897fd8
Erik Brinkman <erik.brinkman@gmail.com>
2022-07-29 19:15:57 -0400

    update deps
    also enable swc transforms

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 // This allows the assets to be reached by chrome
 const isProd = process.env.NODE_ENV === "production";
-export const assetPrefix = isProd ? "/out" : "";
+export const assetPrefix = isProd ? "/out" : "/";
 export const images = { loader: "custom" };
+export const experimental = { forceSwcTransforms: true };
