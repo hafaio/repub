667d4bfb21dd67e357a6722831dadb8f41bcda2f
Erik Brinkman <erik.brinkman@gmail.com>
2025-03-02 11:01:56 -0500

    fix: attempt to fix nav clearing (#18)
    fixes #17

--- src/status.ts
+++ src/status.ts
@@ -1,6 +1,7 @@
 class Tab {
   #tabId: number;
   nav: boolean = false;
+  precount: number = 1;
   count: number = 1;
 
   constructor(tabId: number) {
@@ -61,6 +62,16 @@ class Tab {
     }
   }
 
+  async done(): Promise<void> {
+    this.precount -= 1;
+    if (!this.precount && this.nav) {
+      await chrome.action.setBadgeText({
+        tabId: this.#tabId,
+        text: "",
+      });
+    }
+  }
+
   async drop(): Promise<void> {
     this.count -= 1;
     if (!this.count) {
@@ -83,6 +94,7 @@ export function getTab(tabId: number): Tab {
   if (tab) {
     tab.nav = false;
     tab.count += 1;
+    tab.precount += 1;
     return tab;
   } else {
     const newTab = new Tab(tabId);
@@ -92,19 +104,17 @@ export function getTab(tabId: number): Tab {
 }
 
 // watch for navigation
-chrome.webNavigation.onBeforeNavigate.addListener(
-  ({ tabId, parentDocumentId }) => {
-    if (parentDocumentId === undefined) {
-      // only care about root navigation
-      const active = activeTabs.get(tabId);
-      if (active) {
-        active.nav = true;
-      } else {
-        void chrome.action.setBadgeText({
-          tabId,
-          text: "",
-        });
-      }
+chrome.webNavigation.onCommitted.addListener(({ tabId, frameId }) => {
+  if (frameId === 0) {
+    // only care about root navigation
+    const active = activeTabs.get(tabId);
+    if (active?.precount) {
+      active.nav = true;
+    } else {
+      void chrome.action.setBadgeText({
+        tabId,
+        text: "",
+      });
     }
-  },
-);
+  }
+});
