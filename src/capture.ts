7f585c98ee59a68510dd19452e5e6639bedf9589
Erik Brinkman <erik.brinkman@gmail.com>
2025-11-29 09:41:16 -0500

    fix capture error

--- src/capture.ts
+++ src/capture.ts
@@ -4,11 +4,17 @@ export async function pageCapture(
   { retries = 3 }: { retries?: number } = {},
 ): Promise<ArrayBuffer> {
   for (; retries; --retries) {
-    const blob = await chrome.pageCapture.saveAsMHTML({ tabId });
-    if (blob) {
-      return await blob.arrayBuffer();
-    } else {
-      console.warn(chrome.runtime.lastError);
+    try {
+      // NOTE this occasionally returns null, or throws an error for not being
+      // available, both of these are transient, so we just retry
+      const blob = await chrome.pageCapture.saveAsMHTML({ tabId });
+      if (blob) {
+        return await blob.arrayBuffer();
+      } else {
+        console.warn(chrome.runtime.lastError);
+      }
+    } catch (ex) {
+      console.warn(ex);
     }
   }
   throw new Error("couldn't fetch page");
